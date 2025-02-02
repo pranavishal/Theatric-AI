d1e16d80c3f32b0c517ffb1cb2196256e8e95bf8
Saad Bhutta <saad.mahmood135@gmail.com>
2025-02-02 04:37:45 -0500

    add files

---
+++ backend/services/logline/service.py
@@ -0,0 +1,44 @@
+import openai
+from config.env import must_get_env
+from services.logline.models import LoglineRequest
+
+# Configure OpenAI API key
+openai.api_key = must_get_env("OPEN_API_KEY")
+
+
+def generate_logline(request: LoglineRequest) -> str:
+    messages = [
+        {
+            "role": "system",
+            "content": """
+                       You are a screenwriting assistant. 
+                       You are to help generate a logline based on the information you recive in the prompt. 
+                       If the prompt is empty, generate one completely by yourself.
+                       """,
+        },
+        {"role": "user", "content": f"Create a logline: {request.prompt}"},
+    ]
+    if not request.refinement:
+        response = openai.chat.completions.create(
+            model="gpt-4o", messages=messages, temperature=0.5
+        )
+    else:
+        response = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "system",
+                    "content": """
+                               You are a screenwriting assistant. 
+                               You are to help generate a logline based on the information you recive in the prompt, 
+                               or help refine an existing prompt based on refinement feedback.
+                               """,
+                },
+                {
+                    "role": "user",
+                    "content": f"The current logline is {request.prompt}. Refine it with the following instructions: {request.refinement}",
+                },
+            ],
+            temperature=0.5,
+        )
+    return {"logline": response.choices[0].message.content.strip()}
---
+++ backend/services/synopsis/service.py
@@ -0,0 +1,28 @@
+import openai
+from config.env import must_get_env
+from services.synopsis.models import SynopsisRequest
+
+# Configure OpenAI API key
+openai.api_key = must_get_env("OPEN_API_KEY")
+
+
+def generate_synopsis(request: SynopsisRequest) -> str:
+    messages = [
+        {
+            "role": "system",
+            "content": """
+                       You are a screenwriting assistant. 
+                       You are to help generate a synopsis based on the logline you recieve in the prompt. 
+                       It is to be a few paragraphs in length with the purpose of fleshing out the idea given in the logline. 
+                       Be detailed, introduce characters, setting, and give the user a lot of clarity about the story from them reading your synopsis.
+                       """,
+        },
+        {
+            "role": "user",
+            "content": f"The logline is currently {request.formerPrompt}. Expand on this idea to make it a full synopsis.",
+        },
+    ]
+    response = openai.chat.completions.create(
+        model="gpt-4o", messages=messages, temperature=0.5
+    )
+    return {"synopsis": response.choices[0].message.content}
